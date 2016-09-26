229a2348f87c902afd4872db84181f13e8bf3f96
Robert Murray <rob@wildmonkeybytes.com>
2016-09-26 12:56:37 -0400

    setup development env

--- src/main/java/com/wmb/spark/experiments/SimpleApp.java
+++ src/main/java/com/wmb/spark/experiments/SimpleApp.java
@@ -10,7 +10,7 @@ import org.apache.spark.api.java.function.Function;
  */
 public class SimpleApp {
     public static void main(String[] args) {
-        String logFile = "YOUR_SPARK_HOME/README.md"; // Should be some file on your system
+        String logFile = "sampledatafile.txt"; // Should be some file on your system
         SparkConf conf = new SparkConf().setAppName("Simple Application");
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> logData = sc.textFile(logFile).cache();
