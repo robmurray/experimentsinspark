6c7e1639bd4a0cf68275ea6d72fc4feb03dc9c7a
Robert Murray <rob@wildmonkeybytes.com>
2016-09-30 10:28:49 -0400

    sample scala app to test env config

---
+++ src/main/scala/com/wmb/spark/experments/SimpleScalaApp.scala
@@ -0,0 +1,19 @@
+package org.apache.spark.examples
+
+/**
+  * a simple app to test the scala develop environment configuration
+  */
+
+import scala.math.random
+
+object SimpleScalaApp {
+
+  def main(args: Array[String]) {
+    var results = 0.0
+    for (i <- 1 to 1000000) {
+      val x = random * 3
+      results += x
+    }
+    println("results: "+results)
+  }
+}
